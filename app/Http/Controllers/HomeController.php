2800af467c8c8b7e287b5e18f936216d1aec9de4
yayamow <sellypratiwi300406@gmail.com>
2025-03-09 12:04:07 +0700

    Penambahan file baru

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'nama' => 'Doraemon',
+            'pekerjaan' => 'Developer'
+        ];
+        
+        return view('home')->with($data);
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- It is never too late to be what you might have been. - George Eliot -->
+     Selamat datang di home {{$nama}}<br>
+     Anda adalah seorang {{$pekerjaan}}
+</div>
